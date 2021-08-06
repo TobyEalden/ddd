1920074886cd84caa167185de5fe5d70b8e18d9c
Toby Ealden <toby.ealden@gmail.com>
2021-08-06 16:35:21 +0100

    add inherited device binding view

--- packages/app/components/device-type-bindings.jsx
+++ packages/app/components/device-type-bindings.jsx
@@ -14,19 +14,6 @@ export default function DeviceTypeBindings({deviceTypeId}) {
     return <InfoPanel>No bindings found.</InfoPanel>;
   } else {
     return bindings.data.map((binding, idx) => {
-      // return (
-      //   <div className="border-b-2 py-2">
-      //     <i className="fad fa-link mr-2" />
-      //     {binding.firmware.name}
-      //     <div>
-      //       Bound by '{binding.profile_key_public.profile.name}' at {new Date(binding.signed_at).toLocaleString()} using
-      //       key '{binding.profile_key_public.name}'
-      //     </div>
-      //     {binding.device_type.id !== deviceTypeId && (
-      //       <div className="text-sm">[INHERITED from {binding.device_type.name}]</div>
-      //     )}
-      //   </div>
-      // );
       return (
         <div key={idx} className="border-b-2 py-2">
           <i className="fad fa-microchip mr-2 text-primary-dark" />
